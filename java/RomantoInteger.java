03ec3b3b3a79ee897341de08f486a6161e0ad8b8
Mi Gao <gaomius@hotmail.com>
2018-07-23 18:28:20 -0700

    Roman to Integer

---
+++ java/RomantoInteger.java
@@ -0,0 +1,34 @@
+// Description
+// Given a roman numeral, convert it to an integer.
+
+// The answer is guaranteed to be within the range from 1 to 3999.
+
+class RomantoInteger {
+    public static int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int result = toNumber(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (toNumber(s.charAt(i)) > toNumber(s.charAt(i - 1))) {
+                result += toNumber(s.charAt(i)) - 2 * toNumber(s.charAt(i - 1));
+            } else {
+                res += toNumber(s.charAt(i));
+            }
+        }
+    }
+
+    public static int toNumber(char c) {
+        int result = 0;
+        switch (c) {
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+        }
+        return result;
+    }
+}
