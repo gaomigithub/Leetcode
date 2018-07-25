ed93c027daf9f63b1a26138731add4ecaa3cfb5b
Mi Gao <gaomius@hotmail.com>
2018-07-24 21:29:12 -0700

    MergeSortedLists

---
+++ MergeTwoSortedLists.java
@@ -0,0 +1,46 @@
+import com.sun.deploy.jcp.controller.ESL;
+import com.sun.javafx.tk.DummyToolkit;
+
+// Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+// Example:
+
+// Input: 1->2->4, 1->3->4
+// Output: 1->1->2->3->4->4
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (l1 != null) {
+            tail.next = l2;
+        }
+        if (l2 != null) {
+            tail.next = l1;
+        }
+
+        return dummy.next;
+        
+    }
+}
