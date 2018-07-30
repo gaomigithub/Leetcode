f7a4e162b8288b6e79a2d535c5b235d33ca4f9ab
Mi Gao <gaomius@hotmail.com>
2018-07-29 22:17:04 -0700

    Course Schedule

--- CourseSchedule.java
+++ CourseSchedule.java
@@ -19,6 +19,14 @@
 
 
 // BFS Solution
+/**
+ * 定义二维数组graph来表示这个有向图，一位数组in来表示每个顶点的入度。
+ * 我们开始先根据输入来建立这个有向图，并将入度数组也初始化好。
+ * 然后我们定义一个queue变量，将所有入度为0的点放入队列中，然后开始遍历队列，
+ * 从graph里遍历其连接的点，每到达一个新节点，将其入度减一，
+ * 如果此时该点入度为0，则放入队列末尾。直到遍历完队列中所有的值，
+ * 若此时还有节点的入度不为0，则说明环存在，返回false，反之则返回true。
+ */
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         if(prerequisites == null){
