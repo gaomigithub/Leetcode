8414a62350757316bfbb24d294213bbdfd8f9b7c
Mi Gao <gaomius@hotmail.com>
2018-07-29 21:53:49 -0700

    Graph Valid Tree

---
+++ GraphValidTree.py
@@ -0,0 +1,34 @@
+# Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether these edges make up a valid tree.
+
+# For example:
+
+# Given n = 5 and edges = [[0, 1], [0, 2], [0, 3], [1, 4]], return true.
+
+# Given n = 5 and edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]], return false.
+
+# Hint:
+# 1. Given n = 5 and edges = [[0, 1], [1, 2], [3, 4]], what should your return? Is this case a valid tree?
+# 2. According to the definition of tree on Wikipedia: “a tree is an undirected graph in which any two vertices are connected by exactly one path. In other words, any connected graph without simple cycles is a tree.”
+# 3. Note: you can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+
+class Solution(object):
+    def validTree(self, n, edges):
+        """
+        :typen n: int
+        :type edges: List[List[int]]
+        :rtype: bool
+        """
+        if len(edges) != n-1:
+            return False
+
+        neighbors = {i:[] for i in range(n)}
+
+        for v, w in edges:
+            neighbors[v].append(w)
+            neighbors[w].append(v)
+
+        queue = [0]
+        while queue:
+            queue.extend(neighbors.pop(queue.pop(0), []))
+
+        return not neighbors
