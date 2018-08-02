23ae8bf487b95635e185eb7ba0a891aaec825208
Mi Gao <gaomius@hotmail.com>
2018-08-02 16:06:16 -0700

    NumOfIslands
    注释，BFS

--- NumberofIslands.py
+++ NumberofIslands.py
@@ -21,6 +21,9 @@
 # Output: 3
 
 # DFS
+# 我们遍历矩阵的每一个点，对每个点都尝试进行一次深度优先搜索，如果搜索到1，就继续向它的四周搜索。
+# 同时我们每找到一个1，就将其标为0，
+# 这样就能把整个岛屿变成0。我们只要记录对矩阵遍历时能进入多少次搜索，就代表有多少个岛屿。
 class Solution:
     # @param {boolean[][]} grid a boolean 2D matrix
     # @return {int} an integer
@@ -31,8 +34,8 @@ class Solution:
         row = len(grid)
         col = len(grid[0])
         count = 0
-        for i in xrange(row):
-            for j in xrange(col):
+        for i in range(row):
+            for j in range(col):
                 if grid[i][j] == '1':
                     self.dfs(grid, row, col, i, j)
                     count += 1
@@ -51,3 +54,36 @@ class Solution:
             self.dfs(grid, row, col, x, y - 1)
         if y != col - 1:
             self.dfs(grid, row, col, x, y + 1)
+
+
+# BFS
+class Solution2:
+    # @param grid, a list of list of characters
+    # @return an integer
+    def numIslands(self, grid):
+        ans = 0
+        if not len(grid):
+            return ans
+        m, n = len(grid), len(grid[0])
+        visited = [ [False] * n for x in range(m) ] # m * n
+        for x in range(m):
+            for y in range(n):
+                if grid[x][y] == '1' and not visited[x][y]:
+                    ans += 1
+                    self.bfs(grid, visited, x, y, m, n)
+        return ans
+
+    def bfs(self, grid, visited, x, y, m, n):
+        dz = zip( [1, 0, -1, 0], [0, 1, 0, -1] )
+        queue = [ (x, y) ]
+        visited[x][y] = True
+        while queue:
+            front = queue.pop(0)
+            for p in dz:
+                np = (front[0] + p[0], front[1] + p[1])
+                if self.isValid(np, m, n) and grid[np[0]][np[1]] == '1' and not visited[np[0]][np[1]]:
+                    visited[ np[0] ][ np[1] ] = True
+                    queue.append(np)
+
+    def isValid(self, np, m, n):
+        return np[0] >= 0 and np[0] < m and np[1] >= 0 and np[1] < n
