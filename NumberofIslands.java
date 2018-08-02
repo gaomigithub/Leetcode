2e5d9c601877a3e8bd969846d11f185832128e4c
Mi Gao <gaomius@hotmail.com>
2018-08-02 16:08:46 -0700

    NumOfIslands
    FollowUp，找湖

--- NumberofIslands.java
+++ NumberofIslands.java
@@ -68,3 +68,73 @@ class NumberofIslands {
         }
     }
 }
+
+// 后续 Follow Up
+// Q:如何找湖的数量呢？湖的定义是某个0，其上下左右都是同一个岛屿的陆地。
+// A:我们可以先用Number of island的方法，把每个岛标记成不同的ID，然后过一遍整个地图的每个点，如果是0的话，就DFS看这块连通水域是否被同一块岛屿包围，如果出现了不同数字的陆地，则不是湖。
+
+public class NumberOfLakes {
+    
+    public static void main(String[] args){
+        NumberOfLakes nof = new NumberOfLakes();
+        int[][] world = {{1,1,1,0,1},{1,0,0,1,0},{1,1,1,1,1,},{0,1,1,0,1},{0,1,1,1,1}};
+        System.out.println(nof.numberOfLakes(world));
+    }
+    
+    public int numberOfLakes(int[][] world){
+        int islandId = 2;
+        for(int i = 0; i < world.length; i++){
+            for(int j = 0; j < world[0].length; j++){
+                if(world[i][j] == 1){
+                    findIsland(world, i, j, islandId);
+                    islandId++;
+                }
+            }
+        }
+        int lake = 0;
+        for(int i = 0; i < world.length; i++){
+            for(int j = 0; j < world[0].length; j++){
+                if(world[i][j] == 0){
+                    // 找到当前水域的邻接陆地编号
+                    int currId = 0;
+                    if(i > 0) currId = (world[i - 1][j] != 0 ? world[i - 1][j] : currId);
+                    if(j > 0) currId = (world[i][j - 1] != 0 ? world[i][j - 1] : currId);
+                    if(i < world.length - 1) currId = (world[i + 1][j] != 0 ? world[i + 1][j] : currId);
+                    if(j < world[0].length - 1) currId = (world[i][j + 1] != 0 ? world[i][j + 1] : currId);
+                    // 如果该点是湖，加1
+                    if(findLake(world, i, j, currId)){
+                        lake++;
+                    }
+                }
+            }
+        }
+        return lake;
+    }
+    
+    private boolean findLake(int[][] world, int i, int j, int id){
+        // 将当前水域标记成周边岛屿的数字
+        world[i][j] = id;
+        // 找上下左右是否是同一块岛屿的陆地，如果是水域则继续DFS，如果当前水域是边界也说明不是湖
+        boolean up = i != 0 
+                && (world[i - 1][j] == id 
+                || (world[i - 1][j] == 0 && findLake(world, i - 1, j, id)));
+        boolean down = i != world.length - 1 
+                && (world[i + 1][j] == id 
+                || (world[i + 1][j] == 0 && findLake(world, i + 1, j, id)));
+        boolean left = j != 0 
+                && (world[i][j - 1] == id 
+                || (world[i][j - 1] == 0 && findLake(world, i, j - 1, id)));
+        boolean right = j != world[0].length - 1 
+                && (world[i][j + 1] == id 
+                || (world[i][j + 1] == 0 && findLake(world, i, j + 1, id)));
+        return up && down && right && left;
+    }
+    
+    private void findIsland(int[][] world, int i, int j, int id){
+        world[i][j] = id;
+        if(i > 0 && world[i - 1][j] == 1) findIsland(world, i - 1, j, id);
+        if(j > 0 && world[i][j - 1] == 1) findIsland(world, i, j - 1, id);
+        if(i < world.length - 1 && world[i + 1][j] == 1) findIsland(world, i + 1, j, id);
+        if(j < world[0].length - 1 && world[i][j + 1] == 1) findIsland(world, i, j + 1, id);
+    }
+}
