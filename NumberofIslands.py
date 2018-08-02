90d1f17f8fe6efd14343cdfcacd325ec238a0ad5
Mi Gao <gaomius@hotmail.com>
2018-08-02 16:00:41 -0700

    Number of Islands
    DFS

--- NumberofIslands.py
+++ NumberofIslands.py
@@ -20,25 +20,34 @@
 
 # Output: 3
 
+# DFS
 class Solution:
+    # @param {boolean[][]} grid a boolean 2D matrix
+    # @return {int} an integer
     def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        res = 0
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == "1":
-                    self.dfs(grid, r, c)
-                    res += 1
-        return res
+        if not grid:
+            return 0
 
-    def dfs(self, grid, i, j):
-        dirs = [[-1, 0], [0, 1], [0, -1], [1, 0]]
-        grid[i][j] = "0"
-        for dir in dirs:
-            nr, nc = i + dir[0], j + dir[1]
-            if nr >= 0 and nc >= 0 and nr < len(grid) and nc < len(grid[0]):
-                if grid[nr][nc] == "1":
-                    self.dfs(grid, nr, nc)
+        row = len(grid)
+        col = len(grid[0])
+        count = 0
+        for i in xrange(row):
+            for j in xrange(col):
+                if grid[i][j] == '1':
+                    self.dfs(grid, row, col, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, row, col, x, y):
+        if grid[x][y] == '0':
+            return
+        grid[x][y] = '0'
+
+        if x != 0:
+            self.dfs(grid, row, col, x - 1, y)
+        if x != row - 1:
+            self.dfs(grid, row, col, x + 1, y)
+        if y != 0:
+            self.dfs(grid, row, col, x, y - 1)
+        if y != col - 1:
+            self.dfs(grid, row, col, x, y + 1)
